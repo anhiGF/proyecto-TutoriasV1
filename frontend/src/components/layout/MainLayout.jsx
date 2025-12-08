d2fd09771ed6843ea0bbaec7bed141d6ce0b0fae
anhiGF <153327217+anhiGF@users.noreply.github.com>
2025-12-08 16:25:07 -0600

    BACKEND + FRONTEND

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -33,8 +33,8 @@ export function MainLayout({ children }) {
   navLinks.push({ to: '/', label: 'Inicio' });
 
   if (!isAuthenticated) {
-    navLinks.push({ to: '/login', label: 'Iniciar sesión' });
-          navLinks.push({ to: '/usuarios', label: 'Usuarios' });
+    navLinks.push({ to: '/usuarios', label: 'Usuarios' });
+    //navLinks.push({ to: '/login', label: 'Iniciar sesión' });
   } else {
     navLinks.push({ to: '/perfil', label: 'Mi perfil' });
 
